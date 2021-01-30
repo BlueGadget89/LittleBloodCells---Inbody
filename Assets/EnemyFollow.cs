ca8f7547ddd763ac115456a477f06a55c1626644
Solihah Adeboye <solihahadeboye.sia@gmail.com>
2021-01-30 17:54:27 -0500

    fixing grabbing script and working on enemy following the layer when on the same platform

---
+++ Assets/EnemyFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    private Transform target;
+    public float stoppingDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //enemy finds target with the tag "Player"
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            //move the enemy's position towards the target's position at a certain speed and time
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/GrabController1.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabController1 : MonoBehaviour
+{
+	//bool to check if grabbed is tru/false
+	public bool grabbed;
+	RaycastHit2D hit;
+	//Raycast distance
+	public float distance = 12f;
+	//the position where teh object would stay
+	public Transform holdpoint;	
+	public LayerMask notgrabbed;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//press B to grab the object
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+
+			if (!grabbed)
+			{
+				Physics2D.queriesStartInColliders = false;
+
+				//raycast for both positions so player can detect if object is left or on right				
+				hit = Physics2D.Raycast(transform.position, Vector2.left * transform.localScale.x, distance);			
+
+				//if player grabs the object and object has the tag "Puzzle" make grabbed = true
+				if (hit.collider != null && hit.collider.tag == "Puzzle")
+				{
+					grabbed = true;
+
+				}
+				
+			}
+
+			//if B is pressed let go off item and make grabbed = false
+			else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
+			{
+				grabbed = false;
+
+				if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+				{
+					//hit.collider.gameObject.transform.parent = gameObject.transform;
+					hit.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0f;
+				}				
+			}
+
+		}
+
+		//if the player grabs the object, object shoukd stay at the holder's position
+		if (grabbed)
+        {
+			hit.collider.gameObject.transform.position = holdpoint.position;
+		}
+			
+	}
+	//to see the raycast line
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
+	}
+	
+}
