aaa5bef3f8cd334fef3c1f62d6562c9536a392a9
aaaaa <aaa>
2021-03-13 16:20:06 -0500

    Game Over Screen, Damage Indicators
    There is also now a counter for how many pieces are in the statue, perhaps could be used for a future win screen.

--- Assets/codes/player controls/TwoHeartDestroy.cs
+++ Assets/codes/player controls/TwoHeartDestroy.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class TwoHeartDestroy : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject playerDamageDetector;
+    //Stuff for damage indicator
+    Color damageColor = Color.cyan;
+    Color OriginalColor;
+    SpriteRenderer ColorRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ColorRenderer = GetComponent<SpriteRenderer>();
+        OriginalColor = ColorRenderer.material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerDamageDetector.GetComponent<Player_Damage_Script>().gethit == true)
+        {
+            ColorRenderer.material.color = damageColor;
+        }
+        if (playerDamageDetector.GetComponent<Player_Damage_Script>().gethit == false)
+        {
+            ColorRenderer.material.color = OriginalColor;
+        }
         if (Player.GetComponent<PlayerMovementScript>().playerHp == 1)
         {
             Destroy(gameObject);
