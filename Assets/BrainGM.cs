af70feb248619629308b9001f96787bae2bb26ec
Angus <angushu.sia@gmail.com>
2021-06-05 16:26:48 -0400

    start of the brain level

---
+++ Assets/BrainGM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainGM : MonoBehaviour
+{
+    public GameObject happyplatform, angerplatform,fearplatfrom;
+    public GameObject happyblock, angerblock, fearblock;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
