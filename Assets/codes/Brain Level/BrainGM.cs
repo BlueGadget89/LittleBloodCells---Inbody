ac2a8da4d56265b8745cf7126a40f0d1a70f5d9f
aaaaa <aaa>
2021-06-09 13:09:05 -0400

    Started coding for anger puzzle and transition to anger puzzle

--- Assets/codes/Brain Level/BrainGM.cs
+++ Assets/codes/Brain Level/BrainGM.cs
@@ -8,6 +8,10 @@ public class BrainGM : MonoBehaviour
     public GameObject happyblock, angerblock, fearblock;
     public string emotion;
     public Camera playerCamera, happyCamera, angerCamera, fearCamera;
+    public GameObject emotionGM;
+    public GameObject player;
+    public GameObject playerPositionTracker;
+    public Vector3 playerPos;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,21 @@ public class BrainGM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerPositionTracker.GetComponent<Transform>().position = player.GetComponent<Transform>().position;
+        playerPos = playerPositionTracker.GetComponent<Transform>().position;
         
+        /*
+        if (playerPos.x >= -32.11 && playerPos.x <= 8.06 && player.transform.IsChildOf(transform) == false)
+        {
+            happyCamera.GetComponent<Camera>().enabled = true;
+            playerCamera.GetComponent<Camera>().enabled = false;
+            angerCamera.GetComponent<Camera>().enabled = false;
+            Debug.Log("Player is in the happy puzzle");
+        }
+        */
+        if (emotionGM.GetComponent<BrainPuzzle>().happyComplete == true)
+        {
+            emotion = "Anger";
+        }
     }
 }
